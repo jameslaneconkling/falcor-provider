53b623f3ca2465a1e1778164536cffe0f2aecb05
James Conkling <james.lane.conkling@gmail.com>
2016-10-26 18:46:17 -0400

    update build

--- dist/components/connect.js
+++ dist/components/connect.js
@@ -4,6 +4,8 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _react = require('react');
@@ -23,8 +25,8 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 var id = 0;
 
 exports.default = function (paths, mapStateToProps) {
-  var mapUpdateToProps = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : function (dispatch, $updateGraph) {
-    return { dispatch: dispatch, $updateGraph: $updateGraph };
+  var mapUpdateToProps = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : function (dispatch, falcor, $updateGraph) {
+    return { dispatch: dispatch, falcor: falcor, $updateGraph: $updateGraph };
   };
 
   return function (WrappedComponent) {
@@ -66,11 +68,15 @@ exports.default = function (paths, mapStateToProps) {
       }, {
         key: 'render',
         value: function render() {
-          return _react2.default.createElement(WrappedComponent, Object.assign({}, this.props, mapUpdateToProps(this.context.store.dispatch, this.context.updateGraph()), {
-            updatePaths: this.updatePaths.bind(this),
-            falcor: this.context.falcor,
-            paths: this.context.componentPathMap[this._id]
-          }), this.props.children);
+          return _react2.default.createElement(
+            WrappedComponent,
+            _extends({}, this.props, mapUpdateToProps(this.context.store.dispatch, this.context.falcor, this.context.updateGraph()), {
+              updatePaths: this.updatePaths.bind(this),
+              falcor: this.context.falcor,
+              paths: this.context.componentPathMap[this._id]
+            }),
+            this.props.children
+          );
         }
       }]);
 
